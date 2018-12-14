6a0ac3b5d3801b277b83df0c5929a83a19594a3f
Ada <larmartinez2@gmail.com>
2018-12-14 13:04:58 -0800

    create componentDidMount method to get info from API

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,6 +7,10 @@ import Card from './Card';
 import NewCardForm from './NewCardForm';
 import CARD_DATA from '../data/card-data.json';
 
+//https://inspiration-board.herokuapp.com/boards/Laura/cards?text=hi &emoji=heart_eyes
+
+const URL = 'https://inspiration-board.herokuapp.com/boards/Laura/cards';
+
 class Board extends Component {
   constructor() {
     super();
@@ -16,9 +20,67 @@ class Board extends Component {
     };
   }
 
+  componentDidMount() {
 
+    axios.get(URL)
+      .then((response) => {
+        const cards = response.data.map((card) => {
+          console.log(card.card);
+            const newCard = {
+              ...card.card,
+            }
+          return newCard
+        })
+        this.setState({
+          cards: cards,
+        });
+      })
+      .catch((error) => {
+        console.log(error.message);
+        this.setState({
+          errorMessage: error.message,
+        });
+      });
+
+  }
+
+  addPet = (newPet) => {
+    const apiPayload = {
+      ...newPet,
+      img: newPet.image,
+      breed: newPet.species,
+      owner: '',
+    };
+    axios.post(URL, apiPayload)
+      .then( (response) => {
+        const myNewPet = response.data;
+        newPet.images = [newPet.image];
+        newPet.id = myNewPet.id
+
+        const {petList, masterList} = this.state;
+
+        masterList.push(newPet);
+
+        if (petList !== masterList)
+          petList.push(newPet);
+
+        this.setState({
+          petList,
+          masterList,
+          errorMessage: 'Pet Added',
+        });
+
+      })
+      .catch( (error) => {
+        this.setState({
+          errorMessage: `Failure ${error.message}`,
+        })
+      });
+  }
 
   render() {
+    console.log(this.state.cards)
+
   const cards = CARD_DATA.cards.map((card) => {
       return (
         <Card text = {card.text} emoji= {card.emoji}/>
